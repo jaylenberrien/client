8015f3644c96e2f40e3bb2a7e912e54cf807cde0
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-06-12 21:57:29 -0400

    added alt to img in projects
    added repo link to href in contacts

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -22,7 +22,7 @@ const Contact: React.FC = () => {
       <p className='mb-4 mx-auto w-1/3 text-lg'>My inbox is open to all work and opportunites. You can also just say hi or ask questions if you'd like. Regardless I'll get back to you as soon as I can!</p>
       <p className='inline text-lg'>Email me at </p><a title='Email me'href="mailto:jaylenberrien.jb@gmail.com" className='text-cyan-300'>jaylenberrien.jb@gmail.com </a>
       <button title='Copy to clipboard' className='bg-gray- border-2 px-1 active:bg-gray-200 rounded-sm border-cyan-300 text-cyan-300' onClick={copyEmail}>Copy</button>
-      <p className='mt-20 text-lg'>Made by <a href='' title='See repo for portfolio' className='text-cyan-300'>Jaylen Berrien</a></p>
+      <p className='mt-20 text-lg'>Made by <a href='https://github.com/jaylenberrien/my-portfolio' title='See repo for portfolio' className='text-cyan-300'>Jaylen Berrien</a></p>
     </motion.div>
   )
 }
